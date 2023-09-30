8adb952cc85772dfe03d9359140fdcc1da7b654b
Kei Imada <>
2023-09-30 17:55:54 -0400

    lesson7 - Dead code analysis

--- lesson7/foo.c
+++ lesson7/foo.c
@@ -1,7 +1,9 @@
 int add4(int x) {
+  int y = 0;
+  for (int i = 0; i < 1000000000; i++) {
+    int z = i + i;
+  }
   return x + 4;
 }
 
-int main() {
-  return add4(38);
-}
+int main() { return add4(38); }
---
+++ lesson7/pass/tdce/tdce.cpp
@@ -0,0 +1,45 @@
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Pass.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/Utils/Local.h"
+#include <llvm-14/llvm/Transforms/Utils/Local.h>
+#include <set>
+#include <vector>
+
+using namespace llvm;
+using namespace std;
+
+namespace {
+
+struct DeadCodeAnalysis
+    : public PassInfoMixin<DeadCodeAnalysis> {
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
+    for (auto &F : M) {
+      for (auto &B : F) {
+        for (auto &I : B) {
+          if (llvm::wouldInstructionBeTriviallyDead(&I, nullptr)) {
+            errs() << "WOULD BE DEAD:" << I << "\n";
+          }
+        }
+      }
+    }
+    return PreservedAnalyses::all();
+  };
+};
+
+} // namespace
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return {.APIVersion = LLVM_PLUGIN_API_VERSION,
+          .PluginName = "DCE Pass (Totally not copied)",
+          .PluginVersion = "v0.1",
+          .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
+            PB.registerPipelineStartEPCallback(
+                [](ModulePassManager &MPM, OptimizationLevel Level) {
+                  MPM.addPass(DeadCodeAnalysis());
+                });
+          }};
+}
